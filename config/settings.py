6c767cb7e941ec21aa4138bece0ece72aa01c585
배동수 <baedongsu@baedongsuui-MacBookAir.local>
2023-12-18 02:43:08 +0900

    please

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_URL = 'login'
-RESIDENCE_KEY = b'PjJFCzxlbRNCqOV1g3V3cTP22zm3d1lGp56B5U765M0='
+RESIDENCE_KEY = secrete_settings.SECRET_KEY
+KEY = secrete_settings.SECRET_KEY
 
 # AWS S3 storage setting
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
