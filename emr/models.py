ba5c2a5ab70969443c79e9173630952cbcfbaa0a
배동수 <baedongsu@baedongsuui-MacBookAir.local>
2023-12-17 23:46:16 +0900

    re-changed

--- emr/models.py
+++ emr/models.py
@@ -21,12 +21,13 @@ class PatientIdentity(models.Model):
     patient_name = models.CharField(max_length=60, null=False)
     patient_gender = models.CharField(max_length=1, blank=False, null=False)
     patient_birth = models.CharField(max_length=8, blank=False, null=False)
+    patient_residence_number = models.CharField(max_length=8, blank=False, null=False)
     patient_phone_number = models.CharField(max_length=15, blank=False, null=False)
     patient_emergency_phone_number = models.CharField(max_length=15)
     patient_address = models.CharField(max_length=100)
     patient_agree_essential_term = models.BooleanField(default=False)
     patient_agree_optional_term = models.BooleanField(default=False)
-    patient_residence_number = models.CharField(max_length=8, blank=False, null=False)
+
     class Meta:
         db_table = 'patient_identity'
 
