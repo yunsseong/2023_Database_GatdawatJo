3336f4bfe5f9cb4ae056c43ebe408adf4566e25c
배동수 <baedongsu@baedongsuui-MacBookAir.local>
2023-12-18 00:05:54 +0900

    base64 encode SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 import secrete_settings
+import base64
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,7 +139,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_URL = 'login'
-RESIDENCE_KEY = secrete_settings.SECRET_KEY
+RESIDENCE_KEY = base64.urlsafe_b64encode(secrete_settings.SECRET_KEY)
 
 # AWS S3 storage setting
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
